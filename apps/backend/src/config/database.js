9c84fc3e3563747431f1b776b904d73b1ad4bf66
wellpaper23 <wevelle98@gmail.com>
2026-06-04 16:30:29 -0300

    feat: adicionar configuração de conexão da API com o banco de dados

---
+++ apps/backend/src/config/database.js
@@ -0,0 +1,19 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/index.js";
+import pg from "pg";
+
+
+const user = process.env.DB_USER;
+const senha = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const porta = process.env.DB_PORT;
+const banco = process.env.DB_NAME;
+
+const urlDinamica = `postgresql://${user}:${senha}@${host}:${porta}/${banco}?schema=public`;
+const pool = new pg.Pool({ connectionString: urlDinamica });
+
+const adapter = new PrismaPg(pool);
+console.log("🔍 URL Gerada:", urlDinamica);
+
+const prisma = new PrismaClient({ adapter });
+export { prisma };

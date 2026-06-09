8474652ba561b51ba059635589a66a5a61ff0167
AVELINO <leviaosantos@gmail.com>
2026-06-08 22:24:32 -0300

    feat(SUD-33): adiciona validação de caractere especial na senha

--- apps/frontend/src/app/page.js
+++ apps/frontend/src/app/page.js
@@ -40,6 +40,8 @@ export default function AuthPage() {
   if (senha.length < 8) return 'A senha deve ter pelo menos 8 caracteres.';
   if (!/[A-Z]/.test(senha)) return 'A senha deve conter pelo menos uma letra maiúscula.';
   if (!/[0-9]/.test(senha)) return 'A senha deve conter pelo menos um número.';
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(senha)) {return 'A senha deve conter pelo menos um caractere especial.';
+  }
   return null;
   };
   

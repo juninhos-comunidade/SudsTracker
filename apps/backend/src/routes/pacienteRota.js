57b59a1932d1f8d0d8aefc715cba7dba48dc5c34
wellpaper23 <wevelle98@gmail.com>
2026-06-09 22:24:08 -0300

    feat: adiciona novas rotas para pacientes

--- apps/backend/src/routes/pacienteRota.js
+++ apps/backend/src/routes/pacienteRota.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 const router = Router();
 
 import  PacienteController from "../controllers/pacienteController.js";
-
-router.post("/paciente/buscar", PacienteController.exibirPacientePorId);
-router.post("/paciente/atribuir", PacienteController.alterarProfissional);
+router.get("/", PacienteController.listarPacientes);
+router.get("/:id", PacienteController.exibirPacientePorId);
+router.get("/usuario/:idUsuario", PacienteController.exibirPacientePorUsuario);
+router.get("/profissional/:profissionalId", PacienteController.listarPacientesPorProfissional);
+router.put("/:id/atribuir-profissional", PacienteController.alterarProfissional);
 
 export default router;

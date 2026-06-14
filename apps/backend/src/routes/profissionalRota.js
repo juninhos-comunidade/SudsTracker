6d23c27d8ea034721c11ff100ecd706ab9c56658
wellpaper23 <wevelle98@gmail.com>
2026-06-14 16:34:11 -0300

    feat: adiciona rota para busca de profissional por paciente

--- apps/backend/src/routes/profissionalRota.js
+++ apps/backend/src/routes/profissionalRota.js
@@ -6,5 +6,6 @@ import  ProfissionalController from "../controllers/ProfissionalController.js";
 router.get("/", ProfissionalController.listarProfissionais); 
 router.get("/:id", ProfissionalController.exibirProfissionalPorId);
 router.get("/usuario/:usuarioId", ProfissionalController.exibirProfissionalPorUsuario);
+router.get("/paciente/:pacienteId",ProfissionalController.exibirProfissionalPorPaciente);
 router.put("/:id", ProfissionalController.atualizarProfissional);
 export default router;

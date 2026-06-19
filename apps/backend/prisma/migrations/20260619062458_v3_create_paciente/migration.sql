66a5b1142cbfa5ce85248aa654cd7ca6c2bbd492
JuhCodeSpace <juliabarbosalimadocarmo@gmail.com>
2026-06-19 03:27:01 -0300

    feat(database): adiciona migration do modelo Paciente

---
+++ apps/backend/prisma/migrations/20260619062458_v3_create_paciente/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Paciente" (
+    "id" SERIAL NOT NULL,
+    "idUsuario" TEXT NOT NULL,
+    "idProfissional" INTEGER NOT NULL,
+
+    CONSTRAINT "Paciente_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Paciente_idUsuario_key" ON "Paciente"("idUsuario");
+
+-- AddForeignKey
+ALTER TABLE "Paciente" ADD CONSTRAINT "Paciente_idUsuario_fkey" FOREIGN KEY ("idUsuario") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Paciente" ADD CONSTRAINT "Paciente_idProfissional_fkey" FOREIGN KEY ("idProfissional") REFERENCES "Profissional"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

e6579e7fe9e41b20de9d2d8ea16547b5de9889e2
jonnguii <jg.cravo@outlook.com>
2026-06-25 01:25:24 -0300

    develop: corrge .env

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+  dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 }
 const app = express();
 const PORT = process.env.PORT || 5001;
